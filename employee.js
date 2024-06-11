bea48f6cd2837d82f6b24c3d92498136b08df97b
Robert Weekes <weekes.robert@gmail.com>
2024-06-10 17:41:28 -0700

    done with bind

--- employee.js
+++ employee.js
@@ -1,1 +1,19 @@
-// Your code here 
+class Employee {
+    constructor(name, occupation) {
+        this.name = name;
+        this.occupation = occupation;
+    }
+
+    // INSTANCE METHODS
+    sayName() {
+        console.log(`${this.name} says hello`);
+    }
+
+    sayOccupation() {
+        console.log(`${this.name} is a ${this.occupation}`);
+    }
+
+    // STATIC CLASS METHODS
+}
+
+module.exports = Employee;
--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-// Your code here 
+const Employee = require('./employee');
+
+let john = new Employee('John Wick', 'Dog Lover');
+
+// use bind method on john.sayName to set 'this' context to john
+const sayNameBound = john.sayName.bind(john);
+setTimeout(sayNameBound, 2000);
+
+const sayOccBound = john.sayOccupation.bind(john);
+setTimeout(sayOccBound, 3000);
